02ab3822b8464650ac67112c7be5a61109b39532
SammySheu <samsheu1997@gmail.com>
2022-08-08 17:22:30 +0800

    Add colorMode function and Painting function

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Etch-A-Sketch_Odin-Project
 
+Add colorMode function and painting function on 220808
+
 AddEventListner to button on 220807.
 
 Complete HTML frame on 220802.
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//----------------------- Button Function
 const colorBtn = document.getElementById('colorBtn');
 const rainbowBtn = document.getElementById('rainbowBtn');
 const eraserBtn = document.getElementById('eraserBtn');
@@ -14,9 +16,9 @@ function setCurrentMode(chosen){
 }
 
 function activateButton(chosen){
-  colorBtn.classList.remove('active')
-  rainbowBtn.classList.remove('active')
-  eraserBtn.classList.remove('active')
+  colorBtn.classList.remove('active');
+  rainbowBtn.classList.remove('active');
+  eraserBtn.classList.remove('active');
   if(chosen === 'color')
     colorBtn.classList.add('active') ;
   else if(chosen === 'rainbow')
@@ -24,3 +26,34 @@ function activateButton(chosen){
   else if(chosen === 'eraser')
     eraserBtn.classList.add('active');
 }
+//-----------------------
+
+//----------------------- Grid Function
+const grid = document.querySelector('.grid') ;
+
+function setupGrid(){
+  grid.style.gridTemplateColumns = ' repeat(16, 1fr) ' ;
+  grid.style.gridTemplateRows = ' repeat(16, 1fr) ' ;
+
+  for(let i=0; i<16*16 ; i++){
+    const gridElement = document.createElement('div') ;
+    gridElement.classList.add('grid-element');
+    gridElement.addEventListener('mouseover', painting) ;
+    gridElement.addEventListener('mousedown', painting) ;
+    grid.appendChild(gridElement);
+  }
+}
+setupGrid();
+
+let mouseDown = false ;
+document.body.onmousedown = () => (mouseDown = true) ;
+document.body.onmouseup = () => (mouseDown = false) ;
+
+function painting(e){
+  if (e.type === 'mouseover' && !mouseDown) return ;
+  if(currentMode === 'color'){
+    e.target.style.backgroundColor = '#333333' ;
+  }
+}
+
+//-----------------------
